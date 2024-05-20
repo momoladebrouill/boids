733ab2bb9371cc7e0a423741d0f933cd0004705e
momoladebrouill <momoladebrouill@gmail.com>
2024-05-20 10:14:24 +0200

    Nouveaux coefficients et nettoyage

--- main.ml
+++ main.ml
@@ -5,19 +5,17 @@ let height = 1000
 
 type vec = float * float
 
-type boid = 
-  {
+type boid = {
     position : vec;
     speed : vec;
     color : float;
-  }
+}
 
-type tfac =
-  { 
+type tfac = { 
     cohesion : float;
     alignement : float;
     separation : float;
-  }
+}
 
 type args = {
   boids : boid list;
@@ -25,13 +23,14 @@ type args = {
 }
 
 let seuil = 1e4 (* distance pour observer les autres boids *)
-let speedlimit = 150.0 (* vitesse max des boids*)
+let speedlimit = 300.0 (* vitesse max des boids*)
 let dt = 1.0/.60.0
 
 let zero = 0.0,0.0
 let dist (x1,y1) (x2,y2) = (x1 -. x2)**2.0 +. (y1 -. y2)**2.0
 let norme_squared = dist zero
 let norme a = a |> norme_squared |> sqrt
+let random_centered a = Random.float a -. a/.2.0
 
 let (+$) (x1,y1) (x2,y2) = x1+.x2, y1+.y2
 let (-$) (x1,y1) (x2,y2) = x1-.x2, y1-.y2
@@ -46,29 +45,27 @@ let vect_elem a b =
 let get_angle (x,y) = assert ( norme_squared (x,y) > 0.0) ;  atan2 y x
 let from_angle theta = (cos theta, sin theta)
 
+let around pos = List.filter (fun x -> let d = dist x.position pos in d < seuil) 
+
 let acc pos (ar,n) fac = 
   let separation = 
-    (List.fold_left (+$) zero (List.map (fun x -> vect_elem x.position pos) ar)) *$ fac.separation
+    vect_elem zero (List.fold_left (+$) zero (List.map (fun x -> vect_elem x.position pos) ar))
   in
   let alignement =
     if n = 1.0 then zero else
-    let midangle = List.fold_left (+.) 0.0 (List.map (fun x -> if x.position = pos then 0.0 else get_angle x.speed) ar) /. (n-.1.0) in
-    from_angle midangle *$ fac.alignement
+    let midangle = List.fold_left (+.) 0.0 (List.map (fun x -> if dist x.position pos < 0.1 then 0.0 else get_angle x.speed) ar) /. (n-.1.0) in
+    from_angle midangle 
   in
   let cohesion =
     let midpos = List.fold_left (+$) zero (List.map (fun x-> x.position) ar) /$ n in
-    vect_elem pos midpos *$ fac.cohesion
+    vect_elem pos midpos
   in
-  let acceleration =  cohesion  +$  separation +$ alignement
+  let acceleration =  cohesion *$ fac.cohesion  +$  separation *$ fac.separation +$ alignement *$ fac.alignement
  in
   assert (Float.is_finite (fst acceleration));
   assert (Float.is_finite (snd acceleration));
-
   vect_elem zero acceleration
 
-
-let around pos = 
-  List.filter (fun x -> let d = dist x.position pos in d < seuil) 
 
 let update_boid args {position;speed;color} = 
   (*euler*)
@@ -76,16 +73,18 @@ let update_boid args {position;speed;color} =
     let n = autour |> List.length |> float_of_int in
     let vx,vy = speed +$ acc position (autour,n) args.facteurs in
     let x,y = position in
+    (* colision avec le bord *)
     let speed' = 
       (
-        (if x < 0.0 || x > float_of_int width then -.vx else vx),
-        if y < 0.0 || y > float_of_int height then -.vy else vy
+        (if x <= 0.0 || x >= float_of_int width then -.vx else vx),
+        if y <= 0.0 || y +. 1.0>= float_of_int height then -.vy else vy
      ) in 
+    (* speed limit *)
     let speed' = if norme speed' > speedlimit then vect_elem zero speed' *$ speedlimit else speed' in
     {
       position = position +$ (speed' *$ dt);
       speed = speed'; 
-      color = 0.99 *. color +. 0.01 *. List.fold_left (+.) 0.0 (List.map (fun x-> x.color) autour) /.n  +. Random.float 10.0 -. 5.0 
+      color = Float.rem (0.999 *. color +. 0.001 *. List.fold_left (+.) 0.0 (List.map (fun x-> x.color) autour) /.n  +. random_centered 5.0) 360.0
     }
 
 let update args =
@@ -94,37 +93,34 @@ let update args =
 let demipi = 3.14 /. 2.
 let vec_from_angle (x,y) q alpha =  Vector2.create (x+. q*.cos alpha) (y+.q*.sin alpha) 
 
-let draw_boid others {position;speed;color} =
+let draw_boid {position;speed;color} =
   let theta = get_angle speed  in
   let top = vec_from_angle position 20.0 theta in
   let g = vec_from_angle position 5.0 (theta+.demipi) in
   let d = vec_from_angle position 5.0 (theta-.demipi) in
   draw_triangle top d g (color_from_hsv color 0.5 1.0)
-  (*let x',y' = position+$speed in
-  draw_line (int_of_float x) (int_of_float y) (int_of_float x') (int_of_float y') Color.yellow;
-  List.iter (fun p ->
-    let x',y' = p.position in
-    draw_line (int_of_float x) (int_of_float y) (int_of_float x') (int_of_float y') Color.white) (around position others)
-  *)
 
 
 let draw {boids;facteurs} = 
   begin_drawing ();
-  (*
-  draw_rectangle 0 0 width height (fade Color.black 1.0);
-  draw_rectangle 0 height width 20 Color.black;*)
   clear_background Color.black;
   draw_text 
     (Printf.sprintf "cohesion (A/Z) : %.0f | alignement (Q/S) : %.0f | separation (W/X) : %.0f | space for more boids" facteurs.cohesion facteurs.alignement facteurs.separation)
   0 height 20 Color.white;
-  List.iter (draw_boid boids) boids;
+  List.iter draw_boid boids;
   end_drawing ()
 
 
 let rec loop args = 
   if window_should_close () then close_window () else begin
     draw args;
-    let boids' = if is_key_down Key.Space then ({position = (float_of_int width, float_of_int height) /$ 2.0;speed=Random.float 10.0 -. 5.0,Random.float 10.0 -. 5.0; color = Random.float 360.0 }::args.boids) else args.boids in
+    (* update with user inputs*)
+    let boids' = if is_key_down Key.Space then (
+      {
+        position = (float_of_int width, float_of_int height) /$ 2.0;
+        speed = random_centered 10.0, random_centered 10.0; 
+        color = Random.float 360.0
+      }::args.boids) else args.boids in
     let agrand = 10.0 in
     let fac' = {
       cohesion = args.facteurs.cohesion +. agrand *. ((if is_key_pressed Key.Q then 1.0 else 0.0) +. (if is_key_pressed Key.W then -1.0 else 0.0));
@@ -141,15 +137,15 @@ let () =
   set_target_fps 60;
   let fac =
   {
-    cohesion = 180.0;
-    alignement = 80.0;
-    separation = 10.0;
+    cohesion = 210.0;
+    alignement = 60.0;
+    separation = 170.0;
   } in
   let boids = List.init 50 (fun _ -> 
     {
       position = Random.float (float_of_int width), Random.float (float_of_int height);
-      speed = ((Random.float 10.0 , Random.float 10.0) +$ (-5.0, -5.0)) *$ 1.0;
-      color = Random.float 360.0
+      speed = random_centered 10.0, random_centered 10.0;
+      color = Random.float 360.0;
     })
  in
  loop {boids = boids; facteurs = fac}
